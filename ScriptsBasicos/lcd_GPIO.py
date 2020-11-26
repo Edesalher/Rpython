337180ff1ad05e906a2b3e26786c39ccdecba015
Edesalher <edalher117@gmail.com>
2020-11-25 21:48:00 -0600

    Uso básico de la LCD usando la comunicación paralela con GPIO.

---
+++ ScriptsBasicos/lcd_GPIO.py
@@ -0,0 +1,13 @@
+from RPLCD.gpio import CharLCD
+import RPi.GPIO as GPIO
+
+# Initializing the LCD.
+lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[21, 22, 23, 24], numbering_mode=GPIO.BOARD)
+# Writing data
+lcd.write_string('Test LCD 2x16')
+lcd.cursor_pos = (1, 2)
+lcd.write_string('Raspberry Pi 4')
+
+# Closing the connection
+lcd.close()
+# lcd.close(clear=True)
